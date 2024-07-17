ce9de38e54027a9e730bb54a9672cfd2761230cf
boms03 <dvboms@gmail.com>
2024-07-17 22:01:26 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {LoggingInterceptor} from "./interceptor/logging.interceptor";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
+  const openApiConfig = new DocumentBuilder()
+      .setTitle('Trait Compass')
+      .setDescription('Trait Compass tourism api')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addSecurityRequirements('bearer')
+      .build();
+  const document = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
