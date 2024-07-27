b40d4de6b68f98c79c8b4712e0710a44e92dfa50
boms03 <dvboms@gmail.com>
2024-07-28 01:01:50 +0900

    feat: User module에서 OauthUserController 등록

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,6 +4,8 @@ import {UserController} from "./controller/user.controller";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "./entity/user.entity";
 import {AuthModule} from "../auth/auth.module";
+import {OauthUserController} from "./controller/oauth.user.controller";
+import {OauthUserService} from "./service/oauth.user.service";
 
 @Module({
   imports: [
@@ -12,8 +14,8 @@ import {AuthModule} from "../auth/auth.module";
   ),
       AuthModule
   ],
-  controllers: [UserController],
-  providers: [UserService],
-    exports: [UserService],
+  controllers: [UserController,OauthUserController],
+  providers: [UserService,OauthUserService],
+    exports: [UserService,OauthUserService],
 })
 export class UserModule {}
