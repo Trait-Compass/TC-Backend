7870b52a8439b07c3889efcc2b8073716c066da2
boms03 <dvboms@gmail.com>
2024-09-08 16:59:14 +0900

    feat: 이미지 퀄리티 수정

--- src/modules/gpt/gpt.service.ts
+++ src/modules/gpt/gpt.service.ts
@@ -1,9 +1,5 @@
 import {Injectable, ServiceUnavailableException} from '@nestjs/common';
 import OpenAI from 'openai';
-import {ImageUrl} from "aws-sdk/clients/sesv2";
-import {text} from "express";
-import {ChatCompletionContentPartImage} from "openai/resources";
-import ImageURL = ChatCompletionContentPartImage.ImageURL;
 
 @Injectable()
 export class GptService {
@@ -24,7 +20,7 @@ export class GptService {
             role: 'user',
             content: [
               { type: 'text', text: '이 사진의 무드와 장소를 한글 키워드 3개로 분석해서 오직 배열로만 반환해줘. 다른 설명 없이 ["키워드1", "키워드2", "키워드3"] 형식으로 반환해.' },
-              { type: 'image_url', image_url: { url, detail: 'high' } },
+              { type: 'image_url', image_url: { url, detail: 'low' } },
             ],
           },
         ],
