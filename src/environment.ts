730ae84502cc5db927099055af74465a57770d87
boms03 <dvboms@gmail.com>
2024-09-08 15:24:20 +0900

    chore: 환경 변수 설정명 변경

--- src/environment.ts
+++ src/environment.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export enum Environment {
     MASTER = 'MASTER',
     LOCAL = 'LOCAL',
 }
 
 export const environment: Environment =
-    process.env.ENVIRONMENT as Environment;
+    process.env.NODE_ENV as Environment;
 
 export const isLocal =
     environment === Environment.LOCAL
