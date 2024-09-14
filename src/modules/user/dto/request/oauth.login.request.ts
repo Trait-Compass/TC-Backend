b09cbb8a0977c5f5e39193b96450c5a2717de629
boms03 <dvboms@gmail.com>
2024-09-15 00:59:44 +0900

    hotfix: kakao token

--- src/modules/user/dto/request/oauth.login.request.ts
+++ src/modules/user/dto/request/oauth.login.request.ts
@@ -1,15 +1,57 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsNotEmpty, IsString} from "class-validator";
+import {IsArray, IsNotEmpty, IsString, ValidateNested} from "class-validator";
+import {Type} from "class-transformer";
 
-export class OauthLoginRequest {
+class AccessToken {
     @ApiProperty({
-        example:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuYXV0aDAuY29tLyIsImF1ZCI6Imh0dHBzOi8vYXBpLmV4YW1wbGUuY29tL2NhbGFuZGFyL3YxLyIsInN1YiI6InVzcl8xMjMiLCJpYXQiOjE0NTg3ODU3OTYsImV4cCI6MTQ1ODg3MjE5Nn0.CA7eaHjIHz5NxeIJoFK9krqaeZrPLwmMmgI_XiQiIkQ',
+        example: 'WDXIT44pkLblvWphVoFehbhRlKJQQlncAAAAAQopyV8AAAGR8T7gvOQ1KlcE_6bt',
         description: '엑세스 토큰',
     })
     @IsNotEmpty()
     @IsString()
-    accessToken!: string;
+    access_token!: string;
+
+    @ApiProperty({
+        example: '2024-09-15T12:55:11.209',
+        description: '엑세스 토큰 만료 시간',
+    })
+    @IsNotEmpty()
+    @IsString()
+    expires_at!: string;
+
+    @ApiProperty({
+        example: '_1ve8oLDoiEUZdp4OerBeScw3QSXG1SQAAAAAgopyV8AAAGR8T7guOQ1KlcE_6bt',
+        description: '리프레시 토큰',
+    })
+    @IsNotEmpty()
+    @IsString()
+    refresh_token!: string;
+
+    @ApiProperty({
+        example: '2024-11-14T00:55:11.209',
+        description: '리프레시 토큰 만료 시간',
+    })
+    @IsNotEmpty()
+    @IsString()
+    refresh_token_expires_at!: string;
+
+    @ApiProperty({
+        example: ['account_email', 'profile_nickname'],
+        description: '토큰에 부여된 스코프',
+    })
+    @IsArray()
+    @IsString({ each: true })
+    scopes!: string[];
+}
+export class OauthLoginRequest {
+    @ApiProperty({
+        description: '엑세스 토큰 정보',
+        type: AccessToken
+    })
+    @IsNotEmpty()
+    @ValidateNested()
+    @Type(() => AccessToken)
+    accessToken!: AccessToken;
 
     @ApiProperty({
         example: 'kakao',
