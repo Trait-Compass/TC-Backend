e6f77224801f7e1164af8e05cf885c87933fbbcc
boms03 <dvboms@gmail.com>
2024-09-18 00:11:05 +0900

    feat: 코스 날짜 5일 제한 validator

--- src/decorator/valid.decorator.ts
+++ src/decorator/valid.decorator.ts
@@ -1,5 +1,12 @@
-import {registerDecorator, ValidationArguments, ValidationOptions} from "class-validator";
+import {
+    registerDecorator,
+    ValidationArguments,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface
+} from "class-validator";
 import {SignupRequest} from "../modules/user/dto/request/signup.request";
+import * as dayjs from "dayjs";
 
 export function IsPasswordValid(validationOptions?: ValidationOptions) {
     return function (object: Object, propertyName: string) {
@@ -12,7 +19,7 @@ export function IsPasswordValid(validationOptions?: ValidationOptions) {
                 validate(value: any, args: ValidationArguments) {
                     const signupRequest = args.object as SignupRequest;
                     if (signupRequest.isOauth) {
-                        return true; // Allow empty password for OAuth users
+                        return true;
                     }
                     return value && /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(value);
                 },
@@ -20,6 +27,32 @@ export function IsPasswordValid(validationOptions?: ValidationOptions) {
                     return 'Password too weak';
                 }
             },
+        });
+    };
+}
+
+@ValidatorConstraint({ async: false })
+export class DateRangeConstraint implements ValidatorConstraintInterface {
+    validate(endDate: string, args: any) {
+        const { object } = args;
+        const startDate = object.startDate;
+        const daysDifference = dayjs(endDate).diff(dayjs(startDate), 'day');
+        return daysDifference + 1 <= 5;
+    }
+
+    defaultMessage() {
+        return '날짜 선택은 5일 이하로만 가능합니다';
+    }
+}
+
+export function DateRange(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: DateRangeConstraint,
         });
     };
 }
