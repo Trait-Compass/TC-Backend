939d0583765cadf5885a79d41869fd99753b2585
boms03 <dvboms@gmail.com>
2024-09-21 22:01:39 +0900

    hotfix: 키워드 파싱

--- src/modules/course/query/jCourse.query.ts
+++ src/modules/course/query/jCourse.query.ts
@@ -1,18 +1,23 @@
 import {PcourseQuery} from "./pCourse.query";
 import {Companion, Keyword} from "../../../common/enums";
 import {ApiProperty} from "@nestjs/swagger";
-import {ArrayNotEmpty, IsArray, IsEnum, IsNotEmpty} from "class-validator";
+import {ArrayNotEmpty, IsArray, IsEnum, IsNotEmpty, IsString, Length} from "class-validator";
+import {Transform} from "class-transformer";
 
 export class JcourseQuery extends PcourseQuery {
-    @IsEnum(Keyword, { each: true })
-    @IsArray()
     @ApiProperty({
-        description: '키워드 선택',
-        enum: Keyword,
-        example: ['산', '놀이공원'],
-        isArray: true,
-        type: [String]
+        description: '키워드 선택 (예: 산,놀이공원)',
+        example: '산,놀이공원',
+        type: String
     })
+    @Transform(({ value }) => {
+        if (Array.isArray(value)) {
+            return value.map((item) => item.trim());
+        }
+        return [value.trim()];
+    })
+    @IsArray()
+    @IsEnum(Keyword, { each: true })
     @IsNotEmpty()
     keyword: Keyword[];
 }
